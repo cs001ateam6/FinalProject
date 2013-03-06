7c48da241a5e2fa1c17637d107ec85416e0e056c
Satyen <satyenshanker@yahoo.com>
2013-03-05 19:55:30 -0800

    Innitial changes

---
+++ src/ContactList.java
@@ -0,0 +1,20 @@
+/*
+* Program: ContactList
+*
+* Author: Team 6
+* Written: Mar 05, 2013
+*
+* Course: CS 001A (Java) Winter 2013
+* Assignment: Final Project
+*
+* Compiler: Java 7.0
+* Platform: Windows 
+*
+* Description : This class maintains contacts
+* 	An instance of this class always refers 
+* 	to a contact  
+*/
+
+public class ContactList {
+
+}
---
+++ src/TestContactList.java
@@ -0,0 +1,20 @@
+/*
+* Program: TestList
+*
+* Author: Team 6
+* Written: Mar 05, 2013
+*
+* Course: CS 001A (Java) Winter 2013
+* Assignment: Final Project
+*
+* Compiler: Java 7.0
+* Platform: Windows 
+*
+* Description : This test program performs various use 
+* 			    cases on ContactList class 
+*/
+
+
+public class TestContactList {
+
+}
