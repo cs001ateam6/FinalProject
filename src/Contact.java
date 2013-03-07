15fe82a9c8d7511ece77eb3b596275ca1a7c9711
Elena Morozova <elenamrzva@gmail.com>
2013-03-06 21:49:01 -0800

    Replaced Contact and ContactList with those from Google Doc; added author name to methods I wrote

--- src/Contact.java
+++ src/Contact.java
@@ -1,21 +1,28 @@
-/*
-* Program: Contact
-*
-* Author: Team 6
-* Written: Mar 05, 2013
-*
-* Course: CS 001A (Java) Winter 2013
-* Assignment: Final Project
-*
-* Compiler: Java 7.0
-* Platform: Windows 
-*
-* Description : This program stores contact information
-* An instance of this class stores the information for one contact
-*
-*/
 
-public class Contact {
+/*
+ * Program: Contact
+ *
+ * Author: Team 6
+ * Written: Mar 05, 2013
+ *
+ * Course: CS 001A (Java) Winter 2013
+ * Assignment: Final Project
+ *
+ * Compiler: Java 7.0
+ * Platform: Windows 
+ *
+ * Description : This program stores contact information
+ * An instance of this class stores the information for one contact
+ *
+ */
+
+import java.io.Serializable;
+
+/* 
+ * Implement Serialization so we can write to disk 
+ */
+
+public class Contact implements  Serializable   {
 
 	private String firstName;
 	private String lastName;
@@ -25,71 +32,124 @@ public class Contact {
 	private String phoneNumber;
 	private String addedNotes;
 
+	/*----------------------------------------------------
+	 * Constructors
+	----------------------------------------------------*/
+	
 	/**
-	 * This method is the constructor. It initializes all of this class's private variables to a space,
-	 * so that if one of them is not set and the contact is printed, it will not show.
+	 * This method is the constructor. It initializes all of this class's
+	 * private variables to a space, so that if one of them is not set and the
+	 * contact is printed, it will not show.
 	 */
-	public Contact(){
+	public Contact() {
 		System.out.println("You have initialized all of this contact's variables");
 	}
 
 	/**
-	 * This method sets the contact's firstName to fName, the string passed
-	 * to this method. 
+	 * Satyen: This constructor takes all contact values 
 	 */
-	public void setFirstName(String fName){
+	public Contact(String firstName, String lastName, String streetAddress, String zipCode,
+			String emailAddress, String phoneNumber, String addedNotes) {
+		System.out.println("You have initialized all of this contact's variables");
+	}
+
+	/*----------------------------------------------------
+	 * Properties
+	----------------------------------------------------*/
+
+	/** 
+	 * Satyen: What about the get methods? 
+	 */
+	
+	/**
+	 * Elena:
+	 * This method sets the contact's firstName to fName, the string passed to
+	 * this method.
+	 */
+	public void setFirstName(String fName) {
 		System.out.println("You have called setFirstName() and set the contact's first name");
 	}
 
 	/**
-	 * This method sets the contact's lastName to lName, the string passed 
-	 * to this method.
+	 * Elena:
+	 * This method sets the contact's lastName to lName, the string passed to
+	 * this method.
 	 */
-	public void setLastName(String lName){
+	public void setLastName(String lName) {
 		System.out.println("You have called setLastName() and set the contact's last name");
 	}
 
 	/**
-	 * This method sets the contact's streetAddress to stAddress, the
-	 * string passed to this method.
-	 *
+	 * Elena:
+	 * This method sets the contact's streetAddress to stAddress, the string
+	 * passed to this method.
+	 * 
 	 * It also sets zipCode to the last 5 characters of stAddress.
 	 */
-	public void setStreetAddress(String stAddress){
+	public void setStreetAddress(String stAddress) {
 		System.out.println("You have called setStreetAddress");
 	}
 
 	/**
-	 * This method sets the contact's emailAddress to eAddress, the 
-	 * string passed to this method.
+	 * Elena:
+	 * This method sets the contact's emailAddress to eAddress, the string
+	 * passed to this method.
 	 */
-	public void setEmailAddress(String eAddress){
+	public void setEmailAddress(String eAddress) {
 		System.out.println("You have called setEmailAddress");
 	}
 
 	/**
-	 * This method sets the contact's phoneNumber to phnNumber, the 
-	 * string passed to this method.
+	 * Elena:
+	 * This method sets the contact's phoneNumber to phnNumber, the string
+	 * passed to this method.
 	 */
-	public void setPhoneNumber(String phnNumber){
+	public void setPhoneNumber(String phnNumber) {
 		System.out.println("You have called setPhoneNumber");
 	}
 
 	/**
-	 * This method lets the user add notes to the contact by
-	 * setting addedNotes to the string notes, passed to this method.
+	 * Elena:
+	 * This method lets the user add notes to the contact by setting addedNotes
+	 * to the string notes, passed to this method.
 	 */
-	public void addNotes(String notes){
+	public void addNotes(String notes) {
 		System.out.println("You have called addNotes()");
 	}
 
+	/*----------------------------------------------------
+	 * Other methods
+	----------------------------------------------------*/
+
 	/**
-	 * This method prints the contact's information in a formatted String.
-	 * It returns a String.
+	 * Elena:
+	 * This method prints the contact's information in a formatted String. It
+	 * returns a String.
 	 */
-	public String toString(){
+	public String toString() {
 		System.out.println("You have called toString()");
 		return "You have printed the contact information to String";
 	}
 
+	/*----------------------------------------------------
+	 * Validation methods
+	----------------------------------------------------*/
+
+	/**
+	 * Satyen:
+	 * This method validates the contact name
+	 */
+	private boolean isNameValid() {
+		System.out.println("You have called isNameValid()");
+		return true;
+	}
+
+	/**
+	 * Satyen:
+	 * This method validates the email address
+	 */
+	private boolean isEmailValid() {
+		System.out.println("You have called isEmailValid()");
+		return true;
+	}
 }
--- src/ContactList.java
+++ src/ContactList.java
@@ -1,80 +1,100 @@
 /*
-* Program: ContactList
-*
-* Author: Team 6
-* Written: Mar 05, 2013
-*
-* Course: CS 001A (Java) Winter 2013
-* Assignment: Final Project
-*
-* Compiler: Java 7.0
-* Platform: Windows 
-*
-* Description : This class maintains a contact list
-* 	An instance of this class always refers 
-* 	to an instance of a class Contact
-*/
+ * Program: ContactList
+ *
+ * Author: Team 6
+ * Written: Mar 05, 2013
+ *
+ * Course: CS 001A (Java) Winter 2013
+ * Assignment: Final Project
+ *
+ * Compiler: Java 7.0
+ * Platform: Windows 
+ *
+ * Description : This class maintains a contact list
+ * 	An instance of this class always refers 
+ * 	to an instance of a class Contact
+ */
 
 import java.util.*;
 
 public class ContactList {
 
-	private List<Contact> contactList = new ArrayList<Contact>();
+	/* Satyen: Use an max array constant */
+	private final int MAX_ARRAY_SIZE = 50; 
+	private List<Contact> contactList;
 
+	/*----------------------------------------------------
+	 * Constructors 
+	----------------------------------------------------*/
+	
 	/**
 	 * This is the constructor.
-	 *
-	 */
-	public ContactList(){
-
-	}
-
-	/**
-	 * This method adds a new instance of class Contact to the array contactList
 	 * 
 	 */
-	public void addContact(){
-
+	public ContactList() {
+		/* Satyen: Initialize array to a max size */
+		contactList =  new ArrayList<Contact>(MAX_ARRAY_SIZE);
 	}
 
+	/*----------------------------------------------------
+	 * Retrieval methods
+	----------------------------------------------------*/
 	/**
-	 * This method will return the entire contact list as a string.
-	 *
-	 */
-	public String printContactList(){
-		return "The contact list has been printed";
-	}
-
-	/**
+	 * Elena:
 	 * This method will return all contacts with a given last name.
-	 *
+	 * 
 	 */
-	public String getContactByLastName(String lname){
+	public String getContactByLastName(String lname) {
 		return "Here are the contacts with that last name";
 	}
 
 	/**
+	 * Elena:
 	 * This method will return all contacts with a given email address.
-	 *
+	 * 
 	 */
-	public String getContactByEmail(String email){
+	public String getContactByEmail(String email) {
 		return "Here are the contacts with that email address";
 	}
 
 	/**
+	 * Elena:
 	 * This method will return all contacts with a given zip code.
-	 *
+	 * 
 	 */
-	public String getContactsByZip(String zip){
+	public String getContactsByZip(String zip) {
 		return "Here are the contacts with that zip code";
 	}
 
+	/*----------------------------------------------------
+	 * Other methods
+	----------------------------------------------------*/
+
 	/**
-	 * This method will save the contact list as it has been entered during the current session to disk.
-	 *
+	 * Elena:
+	 * This method adds a new instance of class Contact to the array contactList
+	 * 
 	 */
-	public void saveToDisk(){
+	public void addContact() {
 
 	}
 
+	/**
+	 * Elena:
+	 * This method will return the entire contact list as a string.
+	 * 
+	 */
+	public String printContactList() {
+		return "The contact list has been printed";
+	}
+
+	/**
+	 * Elena:
+	 * This method will save the contact list as it has been entered during the
+	 * current session to disk.
+	 * 
+	 */
+	public void saveToDisk() {
+
+	}
 }
